ae3f96d4e07f4f989a536bfcddc8b1eabdf52671
Gopi K <k.gopi1@gmail.com>
2024-02-03 16:20:00 +0000

    create terraform github workflow

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+    type = string
+    description = "resource_group_name"
+    default = "my-wkl-rg"
+}
+
+variable "location" {
+  type = string
+  description = "Location"
+  default = "westeurope"
+}
